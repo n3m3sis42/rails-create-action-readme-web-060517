c2edd4b996c49119afe43f3564144a4f0a2bad2f
n3m3sis42 <n3m3sis42@gmail.com>
2017-07-05 18:36:52 -0400

    done

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,14 @@
 class PostsController < ApplicationController
+
+	def create
+	  @post = Post.new
+	  @post.title = params[:title]
+	  @post.description = params[:description]
+	  @post.save
+
+		redirect_to post_path(@post)
+	end
+
 	def index
 		@posts = Post.all
 	end
